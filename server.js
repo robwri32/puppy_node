98fcc7ad0f1323f3743a6bb5228e97bfc5c3ca1c
robwri32 <robwri32@gmail.com>
2016-04-28 12:36:42 -0400

    homepage partially setup, minor morning tweaks

--- server.js
+++ server.js
@@ -19,36 +19,24 @@ app.use(bodyParser.json())
 app.use(express.static('public'))
 
 app.get('/', function(req, res) {
-  db.collection('quotes').find().toArray(function(err, result) {
+  db.collection('puppies').find().toArray(function(err, result) {
     if (err) return console.log(err)
-    res.render('index.ejs', {quotes: result})
+    console.log(result)
+    result.sort(function(a, b) {
+      if (a.likes > b.likes) {
+        return -1;
+      }
+      if (a.likes < b.likes) {
+        return 1;
+      }
+      return 0;
+    });
+
+
+    res.render('index.ejs', {puppies: result})
+
   })
 })
-
-app.post('/quotes', function(req, res) {
-  db.collection('quotes').save(req.body, function(err, result){
-    if (err) return console.log(err)
-    console.log('saved to database')
-    res.redirect('/')
-  })
-})
-
-app.put('/quotes', function(req, res) {
-  db.collection('quotes')
-  .findOneAndUpdate({name: 'Yoda'}, {
-    $set: {
-      name: req.body.name,
-      quote: req.body.quote
-    }
-  }, {
-    sort: {_id: -1},
-    upsert: true
-  }, function(err, result) {
-    if (err) return res.send(err)
-    res.send(result)
-  })
-})
-
 
 
 app.get('/add_user', function(req, res){
